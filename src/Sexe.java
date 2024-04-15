320d21a98027c4fcde176b7db1a1d8dba2fd1d7e
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-04-15 11:23:06 +0200

    Ajout de l'enum Sexe

---
+++ src/Sexe.java
@@ -0,0 +1,22 @@
+public enum Sexe {
+    HOMME("M."),
+    FEMME("Mme.");
+
+    private String abreviation;
+
+    /**
+     * Constructeur de l'énumération Sexe
+     * @param abreviation l'abréviation du sexe
+     */
+    Sexe(String abreviation) {
+        this.abreviation = abreviation;
+    }
+
+    /**
+     * Retourne l'abréviation du sexe
+     * @return l'abréviation du sexe
+     */
+    public String getAbreviation() {
+        return abreviation;
+    }
+}
