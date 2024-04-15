aa418e6522fd4dd77406a654c0e220c47a9d5a80
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-04-15 11:24:40 +0200

    Ajout de la documentation dans Participant

--- src/Participant.java
+++ src/Participant.java
@@ -1,7 +1,15 @@
 public interface Participant {
+
+    /**
+     * Méthode permettant à un participant de participer à une compétition.
+     * @return le score du participant
+     */
+    public int participer();
     
-    public void participer();
-    
+    /**
+     * Méthode permettant d'obtenir le nom du participant.
+     * @return le nom du participant
+     */
     public String obtenirNom();
 } 
     
