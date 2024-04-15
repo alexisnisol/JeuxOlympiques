41933e1d925208ecca8f5bb48268283c45befe5e
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-04-15 11:50:50 +0200

    Refactor de JeuxOlympiques

--- src/JeuxOlympiques.java
+++ src/JeuxOlympiques.java
@@ -3,7 +3,8 @@ import java.util.List;
 
 public class JeuxOlympiques {
 
-    private int annee, nbEpreuves;
+    private int annee;
+    private int nbEpreuves;
     private List<Competition> lesCompetitions;
 
     public JeuxOlympiques(int annee, int nbEpreuves) {
@@ -11,90 +12,84 @@ public class JeuxOlympiques {
         this.nbEpreuves = nbEpreuves;
         this.lesCompetitions = new ArrayList<>();
     }
+
+    /**
+     * Cette fonction permet de retourner l'année des jeux olympiques
+     * @return l'année des jeux olympiques
+     */
     public int getAnnee() {
         return annee;
     }
 
+    /**
+     * Cette fonction permet de retourner le nombre d'épreuves des jeux olympiques
+     * @return le nombre d'épreuves des jeux olympiques
+     */
     public int getNbEpreuves() {
         return nbEpreuves;
     }
 
+    /**
+     * Cette fonction permet de retourner la liste des compétitions des jeux olympiques
+     * @return la liste des compétitions des jeux olympiques
+     */
     public List<Competition> getLesCompetitions() {
         return lesCompetitions;
     }
     
-
-    public void demarrerCompetition(){
     /**
      * Cette Fonction permet de demarrer une competition de la liste de compétitions
      * 
      */
+    public void demarrerCompetition(){
+
     }
 
-    
+    /**
+     * Cette fonction permet d'enregistrer le résultat d'un participant dans les jeux olympiques
+     * @param participant : participant d'une competition ou du jeux olympiques
+     */
     public void enregistrerResultat(Participant participant){
-        /**
-         * Cette fonction permet d'enregistrer le résultat d'un participant dans les jeux olympiques
-         * 
-         * <p>
-         * @param participant : participant d'une competition ou du jeux olympiques
-         */
+        
     }
     
-    
+    /**
+     * La fonction permet de recuperer le ou les résultats de l'athlète dans toute les compétitions auquelles il a participer
+     * @param Participant : le participant que l'on souhaite connaître le résultat 
+     * @return on retourne donc ses résultats dans toute ses compétitions
+     */
     public int recupererResultat(Participant participant){
-        /**
-         * La fonction permet de recuperer le ou les résultats de l'athlète dans toute les compétitions auquelles il a participer
-         * @param Participant : le participant que l'on souhaite connaître le résultat 
-         * @return on retourne donc ses résultats dans toute ses compétitions
-         */
         return 0;
-
     }
 
+    /**
+     * Cette fonction permet d'enregistrer une competition dans les jeux olympiques
+     * @param competition : competition que l'on souhaite ajouter dans les jeux olympiques 
+     */
     public void enregistrerCompetition(Competition competition){
-        /**
-         * Cette fonction permet d'enregistrer une competition dans les jeux olympiques
-         * @param competition : competition que l'on souhaite ajouter dans les jeux olympiques 
-         */
-
-         lesCompetitions.add(competition);
+        lesCompetitions.add(competition);
     }
 
     public void enregistrerParticipant(Participant participant, Competition competition){
-        // throws et throw à faire 
+        //TODO: throws et throw à faire 
         for (Competition competition1 : lesCompetitions) {
-            if (competition == competition1){
+            if (competition.equals(competition1)){ //TODO : equals à faire dans Competition
                 competition.getParticipants().add(participant);
             }
         }
     }
 
+    /**
+     * Cette fonction permet de retourner le classement des participants par les médailles
+     */
     public List<Participant> classementMedailles(){
         return null;
-        
     }
 
+    /**
+     * Cette fonction permet de retourner le classement des participants ayant des médailles d'or
+     */
     public List<Participant> classementMedaillesOr(){
         return null;
-        
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (!(obj instanceof JeuxOlympiques)) {
-            return false;
-        }
-
-        JeuxOlympiques jeux = (JeuxOlympiques) obj;
-        
-        return jeux.annee == this.annee && jeux.nbEpreuves == this.nbEpreuves && jeux.lesCompetitions == this.lesCompetitions;
-
-    }
-
-
-    
+    }    
 }
