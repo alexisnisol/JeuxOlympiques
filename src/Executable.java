3c3b2b00c945e5e01c7b689394a3379fd393ab09
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-04-15 12:27:29 +0200

    Ajout de l'Executable

---
+++ src/Executable.java
@@ -0,0 +1,9 @@
+public class Executable {
+
+    public static void main(String[] args) {
+        JeuxOlympiques jo = new JeuxOlympiques(2024, 10);
+        System.out.println(jo);
+
+        System.out.println(JeuxOlympiques.fromCsv("donnees.csv"));
+    }
+}
