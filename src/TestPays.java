e194a510b4157cf061a118b16ef35b8575df5035
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-05-06 11:43:22 +0200

    Implémentation des tests de Pays

---
+++ src/TestPays.java
@@ -0,0 +1,49 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestPays {
+    private Equipes equipe1;
+    private Equipes equipe2;
+    private Equipes equipe3;
+    private Equipes equipe4;
+    private Athletes athlete1;
+    private Athletes athlete2;
+    private Pays france;
+    private Pays usa;
+
+    @Before
+    public void setUp() {
+        france = new Pays("France");
+        usa = new Pays("Etats Unis");
+
+        equipe1 = new Equipes("Équipe de test", 5, false, "", 0, france);
+        equipe2 = new Equipes("Équipe de test 2", 10, true, "", 0, france);
+        equipe3 = new Equipes("Équipe de test 3", 5, false, "", 0, france);
+        equipe4 = new Equipes("Équipe de test 4", 10, true, "", 0, usa);
+        athlete1 = new Athletes("Doe", "John", Sexe.HOMME, 50, 60, 70, "", 0, france, equipe1);
+        athlete2 = new Athletes("Test", "Test", Sexe.FEMME, 30, 35, 40, "", 0, usa, equipe4);
+    }
+
+
+    @Test
+    public void testEquipes() {
+        assertEquals(Arrays.asList(equipe1, equipe2, equipe3), france.getEquipes());
+        assertEquals(Arrays.asList(equipe4), usa.getEquipes());
+    }
+
+    @Test
+    public void testAthletes() {
+        assertEquals(Arrays.asList(athlete1), france.getAthletes());
+        assertEquals(Arrays.asList(athlete2), usa.getAthletes());
+    }
+
+
+    @Test
+    public void testObtenirEquipe() {
+    }
+}
