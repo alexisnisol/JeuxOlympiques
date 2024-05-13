56947cda667df65059b3ac79549a85fae8e0d594
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-05-13 12:09:33 +0200

    Ajout du test de rejoindreEquipe de Athletes

--- src/TestEquipes.java
+++ src/TestEquipes.java
@@ -8,20 +8,24 @@ import java.util.List;
 public class TestEquipes {
     private Equipes equipe;
     private Athletes athlete1;
+    private Athletes athlete1bis;
     private Athletes athlete2;
+    private Athletes athlete3;
     private Pays pays;
 
     @Before
     public void setUp() {
         pays = new Pays("France");
-        equipe = new Equipes("Équipe de test", 5, false, "", 0, pays);
+        equipe = new Equipes("Équipe de test", 2, false, "", 0, pays);
         athlete1 = new Athletes("Doe", "John", Sexe.HOMME, 50, 60, 70, "", 0, pays);
+        athlete1bis = new Athletes("Doe", "John", Sexe.HOMME, 50, 60, 70, "", 0, pays);
         athlete2 = new Athletes("DoeDoe", "JohnJohn", Sexe.HOMME, 50, 60, 70, "", 0, pays);
+        athlete3 = new Athletes("DoeDoeDoe", "JohnJohnJohn", Sexe.HOMME, 50, 60, 70, "", 0, pays);
     }
 
     @Test
     public void testGetTailleMax() {
-        assertEquals(5, equipe.getTailleMax());
+        assertEquals(2, equipe.getTailleMax());
     }
 
     @Test
@@ -39,6 +43,15 @@ public class TestEquipes {
     public void testSetEnRelais() {
         equipe.setEnRelais(true);
         assertTrue(equipe.isEnRelais());
+    }
+
+    @Test
+    public void testRejoindreEquipe() {
+        assertTrue(athlete1.rejoindreEquipe(equipe));
+        assertFalse(athlete1.rejoindreEquipe(equipe)); // ParticipantDejaPresentException
+        assertFalse(athlete1bis.rejoindreEquipe(equipe)); // ParticipantDejaPresentException : même si l'objet est différent, il est considéré comme identique à l'aide de la méthode equals.
+        assertTrue(athlete2.rejoindreEquipe(equipe));
+        assertFalse(athlete3.rejoindreEquipe(equipe)); // EquipePleineException
     }
 
     @Test
