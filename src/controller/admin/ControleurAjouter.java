def36931ae95e0793173ea66cfad132732e7eab1
alexisnisol <alexis.nisol@etu.univ-orleans.ffr>
2024-06-20 00:15:41 +0200

    Ajout Alert si champs vide à l'ajout par Admin

--- src/controller/admin/ControleurAjouter.java
+++ src/controller/admin/ControleurAjouter.java
@@ -1,9 +1,5 @@
 package controller.admin;
 
-import java.util.Arrays;
-import java.util.List;
-
-import BD.RequetesJDBC;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
@@ -14,10 +10,8 @@ import modele.exceptions.MauvaisParticipantException;
 import modele.exceptions.ParticipantDejaPresentException;
 import modele.exceptions.ParticipantOccupeException;
 import modele.exceptions.SexeCompetitionException;
-import modele.participants.Participant;
 import modele.participants.Athlete;
 import modele.participants.Equipe;
-import modele.sports.Sport;
 import modele.Pays;
 import vue.administrateur.Ajouter.TypeAjout;
 import vue.administrateur.Ajouter;
@@ -43,69 +37,89 @@ public class ControleurAjouter implements EventHandler<ActionEvent> {
                 Pays pays = this.modele.getPaysFromString(this.ajout.getPays().getText());
                 
 
-                try {
-                    Athlete athlete = new Athlete(
-                            ajout.getNom().getText(),
-                            ajout.getPrenom().getText(),
-                            ajout.getSexe(),
-                            ajout.getForce(),
-                            ajout.getEndurance(),
-                            ajout.getAgilite(),
-                            pays);
-
-                    if(!this.ajout.athleteDansUneEquipe()){
-
-                        Competition competition = ajout.getEpreuvesAthletesBox().getValue();
-                        try {
-                            competition.enregistrerParticipant(athlete);
-                            this.ajout.addPopup("L'athlète " + athlete.obtenirNom() + " a bien été ajouté.").showAndWait();
-                            this.ajout.resetTF();
-
-                        } catch (SexeCompetitionException | ParticipantDejaPresentException | ParticipantOccupeException | MauvaisParticipantException exception) {
-                            exception.printStackTrace();
-                            Alert alert = new Alert(AlertType.ERROR);
-                            alert.setTitle("Erreur");
-                            alert.setHeaderText("Erreur dans la compétition");
-                            alert.setContentText(exception.getMessage());
-                            alert.showAndWait();
-                        }
-                    }else{
-
-                        if(this.ajout.getEquipesBox().getValue() == null){
-                            Alert alert = new Alert(AlertType.ERROR);
-                            alert.setTitle("Erreur");
-                            alert.setHeaderText("Erreur lors de la récupération des données");
-                            alert.setContentText("L'équipe n'existe pas");
-                            alert.showAndWait();
-                            return;
-                        }
-
-                        Equipe equipe = this.ajout.getEquipesBox().getValue();
-
-                        if(equipe.obtenirPays().equals(pays)){
-                            athlete.rejoindreEquipe(equipe);
-                            this.ajout.addPopup("L'athlète " + athlete.obtenirNom() + " a rejoint l'équipe.").showAndWait();
-                            this.ajout.resetTF();                            
-                        }else{
-                            Alert alert = new Alert(AlertType.ERROR);
-                            alert.setTitle("Erreur");
-                            alert.setHeaderText("Erreur lors de la récupération des données");
-                            alert.setContentText("Erreur dans le pays");
-                            alert.showAndWait();
-                        }
-
+                try{
+                    if (this.ajout.getNom().getText().isEmpty() || this.ajout.getPrenom().getText().isEmpty() || this.ajout.getPays().getText().isEmpty() || (this.ajout.getEpreuvesAthletesBox().getValue() == null && !this.ajout.athleteDansUneEquipe())) {
+                        Alert alert = new Alert(AlertType.ERROR);
+                        alert.setTitle("Erreur");
+                        alert.setHeaderText("Erreur lors de la récupération des données");
+                        alert.setContentText("Une donnée est manquante");
+                        alert.showAndWait();
+                        return;
                     }
-                } catch (NumberFormatException e) {
+                }catch(NumberFormatException e){
                     Alert alert = new Alert(AlertType.ERROR);
                     alert.setTitle("Erreur");
                     alert.setHeaderText("Erreur lors de la récupération des données");
-                    alert.setContentText(e.getMessage());
+                    alert.setContentText("Les champs force, endurance et agilité doivent être des nombres entiers.");
                     alert.showAndWait();
+                    return;
                 }
+
+            
+                Athlete athlete = new Athlete(
+                        ajout.getNom().getText(),
+                        ajout.getPrenom().getText(),
+                        ajout.getSexe(),
+                        ajout.getForce(),
+                        ajout.getEndurance(),
+                        ajout.getAgilite(),
+                        pays);
+
+                if(!this.ajout.athleteDansUneEquipe()){
+
+                    Competition competition = ajout.getEpreuvesAthletesBox().getValue();
+                    try {
+                        competition.enregistrerParticipant(athlete);
+                        this.ajout.addPopup("L'athlète " + athlete.obtenirNom() + " a bien été ajouté.").showAndWait();
+                        this.ajout.resetTF();
+
+                    } catch (SexeCompetitionException | ParticipantDejaPresentException | ParticipantOccupeException | MauvaisParticipantException exception) {
+                        exception.printStackTrace();
+                        Alert alert = new Alert(AlertType.ERROR);
+                        alert.setTitle("Erreur");
+                        alert.setHeaderText("Erreur dans la compétition");
+                        alert.setContentText(exception.getMessage());
+                        alert.showAndWait();
+                    }
+                }else{
+
+                    if(this.ajout.getEquipesBox().getValue() == null){
+                        Alert alert = new Alert(AlertType.ERROR);
+                        alert.setTitle("Erreur");
+                        alert.setHeaderText("Erreur lors de la récupération des données");
+                        alert.setContentText("L'équipe n'existe pas");
+                        alert.showAndWait();
+                        return;
+                    }
+
+                    Equipe equipe = this.ajout.getEquipesBox().getValue();
+
+                    if(equipe.obtenirPays().equals(pays)){
+                        athlete.rejoindreEquipe(equipe);
+                        this.ajout.addPopup("L'athlète " + athlete.obtenirNom() + " a rejoint l'équipe.").showAndWait();
+                        this.ajout.resetTF();                            
+                    }else{
+                        Alert alert = new Alert(AlertType.ERROR);
+                        alert.setTitle("Erreur");
+                        alert.setHeaderText("Erreur lors de la récupération des données");
+                        alert.setContentText("Erreur dans le pays");
+                        alert.showAndWait();
+                    }
+
+                }
+                
                 break;
             case AjoutEquipe:
 
                 Competition competition = ajout.getEpreuveEquipes();
+                if(competition == null || this.ajout.getNomEquipe().getText().isEmpty() || this.ajout.getPaysEquipe().getText().isEmpty()){
+                    Alert alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Erreur");
+                    alert.setHeaderText("Erreur lors de la récupération des données");
+                    alert.setContentText("Une donnée est manquante");
+                    alert.showAndWait();
+                    return;
+                }
                 
                 Pays paysEquipe = this.modele.getPaysFromString(this.ajout.getPaysEquipe().getText());
                 
@@ -119,11 +133,6 @@ public class ControleurAjouter implements EventHandler<ActionEvent> {
                     this.ajout.addPopup("L'équipe " + equipe.obtenirNom() + " a bien été ajouté.").showAndWait();
                     this.ajout.resetTF();
 
-                    /*for (Competition compet : ajout.getMain().getCompetitions()){
-                        if (compet.getSport() == competition.getSport()){
-                            compet.enregistrerParticipant(athlete);
-                        }
-                    }*/
                 } catch (SexeCompetitionException | ParticipantDejaPresentException | ParticipantOccupeException | MauvaisParticipantException exception) {
                     exception.printStackTrace();
                     Alert alert = new Alert(AlertType.ERROR);
